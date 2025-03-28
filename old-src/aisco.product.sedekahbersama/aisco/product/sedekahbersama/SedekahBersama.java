38881688386cbcae435eab64a0bc0ef1f6e6c0fd
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-03-28 23:21:31 +0700

    Added sedekahbersama to old-src if needed

--- old-src/aisco.product.sedekahbersama/aisco/product/sedekahbersama/SedekahBersama.java
+++ old-src/aisco.product.sedekahbersama/aisco/product/sedekahbersama/SedekahBersama.java
@@ -1,26 +1,56 @@
 package aisco.product.sedekahbersama;
 
-import aisco.program.ProgramFactory;
-import aisco.program.core.Program;
-import aisco.financialreport.FinancialReportFactory;
-import aisco.financialreport.core.FinancialReport;
 import aisco.donation.DonationFactory;
 import aisco.donation.core.Donation;
+import aisco.financialreport.FinancialReportFactory;
+import aisco.financialreport.core.FinancialReport;
+import aisco.financialreport.income.FinancialReportImpl;
+import aisco.program.ProgramFactory;
+import aisco.program.core.Program;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SedekahBersama {
-	private static Program activityProgram;
-	private static FinancialReport financialreportFinancialReport;
-	private static FinancialReport incomeFinancialReport;
-	private static Donation donationDonation;
-	private static Donation pgatewayDonation;
-	private static Donation creditcardDonation;
-
-    public static void main(String[] args) {    	
-        // TODO: Create necessary feature objects for the program
-        // TODO: Implement the main logic of the program
-        // TODO: Handle input and output if needed
-        // TODO: Add exception handling if necessary
-        
-        
-    }
+  private static final int INDEX_DISASTER = 0;
+  
+  private static final int INDEX_MEDICINE = 1;
+  
+  private static FinancialReport income1;
+  
+  public static List<Program> addProgram() {
+    System.out.println("\n Programs: ");
+    ArrayList<Program> arrayList = new ArrayList();
+    Program program1 = ProgramFactory.createProgram("aisco.program.activity.ProgramImpl", new Object[] { Integer.valueOf(1), "Flood in Bogor", "Helping flood victims", "100 families", "Fast Furious", "https://www.hilfe.splelive.id/logohilfe" });
+    Program program2 = ProgramFactory.createProgram("aisco.program.activity.ProgramImpl", new Object[] { Integer.valueOf(2), "Free Medicine", "Free treatment and medicine for poor people", "50 persons", "Healthy Clinic", "https://www.hilfe.splelive.id/logopengobatan" });
+    arrayList.add(0, program1);
+    arrayList.add(1, program2);
+    return arrayList;
+  }
+  
+  public static List<FinancialReport> addIncome(List<Program> paramList) {
+    ArrayList<FinancialReport> arrayList = new ArrayList();
+    income1 = FinancialReportFactory.createFinancialReport("aisco.financialreport.income.FinancialReportImpl", new Object[] { FinancialReportFactory.createFinancialReport("aisco.financialreport.core.FinancialReportImpl", new Object[] { "1", "23-11-2019", Integer.valueOf(1000000), "Donation Nadia", paramList.get(0), "42010" }), "Transfer" });
+    arrayList.add(income1);
+    arrayList.add(FinancialReportFactory.createFinancialReport("aisco.financialreport.income.FinancialReportImpl", new Object[] { FinancialReportFactory.createFinancialReport("aisco.financialreport.core.FinancialReportImpl", new Object[] { "2", "25-11-2019", Integer.valueOf(800000), "Donation Lentera", paramList.get(1), "42010" }), "Cash" }));
+    arrayList.add(FinancialReportFactory.createFinancialReport("aisco.financialreport.income.FinancialReportImpl", new Object[] { FinancialReportFactory.createFinancialReport("aisco.financialreport.core.FinancialReportImpl", new Object[] { "3", "29-11-2019", Integer.valueOf(5000000), "Fundraising Mitra University", paramList.get(0), "42000" }), "Transfer" }));
+    return arrayList;
+  }
+  
+  public static void addDonation() {
+    Donation donation = DonationFactory.createDonation("aisco.donation.creditcard.DonationImpl", new Object[0]);
+    donation.addDonation();
+    donation.getDonation();
+  }
+  
+  public static void main(String[] paramArrayOfString) {
+    System.out.println("Product Sedekah Bersama");
+    List<Program> list = addProgram();
+    System.out.println(list);
+    List<FinancialReport> list1 = addIncome(list);
+    income1.printHeader();
+    System.out.println(list1);
+    int i = ((FinancialReportImpl)income1).total(list1);
+    addDonation();
+    System.out.println("Balance: " + i);
+  }
 }
