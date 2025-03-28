a88e03304920efd4205c9889183f6f14a091bcf4
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-03-28 09:32:56 +0700

    initial commit

---
+++ src/aisco.product.charityschool/module-info.java
@@ -0,0 +1,7 @@
+module aisco.product.charityschool {
+    requires aisco.program.core;
+    requires aisco.program.operational;
+    requires aisco.financialreport.core;
+    requires aisco.financialreport.income;
+    requires aisco.financialreport.expense;
+}
